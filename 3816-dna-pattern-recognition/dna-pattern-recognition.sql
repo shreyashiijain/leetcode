9dfb93cc33f483bb8ba742abc517e43f41739ad6
Shreyashi Jain <shreyashi2292@gmail.com>
2026-09-18 16:37:18 +0530

    Time: 248 ms (99.96%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3816-dna-pattern-recognition/dna-pattern-recognition.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select sample_id, dna_sequence, species,
+case when dna_sequence like 'ATG%' then 1 else 0 end as has_start,
+case when dna_sequence like '%TAA' or dna_sequence like '%TGA' or dna_sequence like '%TAG' then 1 else 0 end as has_stop,
+case when dna_sequence like'%ATAT%' then 1 else 0 end as has_atat,
+case when dna_sequence like '%GGG%' then 1 else 0 end as has_ggg
+from samples
+order by sample_id;
