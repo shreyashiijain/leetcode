4c48e3f39c74ae7875d96d65ef9385b76027ecce
Shreyashi Jain <shreyashi2292@gmail.com>
2026-09-19 12:25:14 +0530

    Time: 365 ms (37.52%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,10 @@
+WITH all_ids AS (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted
+)
+SELECT id, COUNT(*) AS num
+FROM all_ids
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
